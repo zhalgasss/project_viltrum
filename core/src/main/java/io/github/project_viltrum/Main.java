360229543d48cc895f983b01485b90ac0824e926
zhalgasss <zhalgaszh07@gmail.com>
2026-05-09 07:25:25 +0500

    hitbox damage animation wave banner bullets walking animation

---
+++ core/src/main/java/io/github/project_viltrum/Main.java
@@ -0,0 +1,8 @@
+package io.github.project_viltrum;
+
+/**
+ * Compatibility wrapper for old IntelliJ/libGDX run configurations.
+ * The real game lives in com.project.viltrum.Main.
+ */
+public class Main extends com.project.viltrum.Main {
+}
---
+++ lwjgl3/src/main/java/io/github/project_viltrum/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,27 @@
+package io.github.project_viltrum.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.project.viltrum.Main;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+    public static void main(String[] args) {
+        if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("Invincible: Fall of Earth");
+        configuration.setWindowedMode(1600, 900);
+        configuration.setForegroundFPS(60);
+        configuration.useVsync(true);
+
+        return configuration;
+    }
+}
